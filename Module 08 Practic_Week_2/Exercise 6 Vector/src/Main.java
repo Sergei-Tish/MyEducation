f2faac0114d30b65abae78e48c72ef0b241c427f
Sergey Tishchenko <seti.volch@gmail.com>
2023-01-30 20:47:24 +0700

    complete exercise 6

--- Module 08 Practic_Week_2/Exercise 6 Vector/src/Main.java
+++ Module 08 Practic_Week_2/Exercise 6 Vector/src/Main.java
@@ -1,12 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        Vector2D vector2D = new Vector2D(-4, 2, 9, 3);
+        Vector2D vector2D = new Vector2D(0, 0, 1, 1);
+        Vector2D vector2D1 = new Vector2D(0, 0, 3, 2);
         System.out.println(vector2D.getVectorCords());
         System.out.println(vector2D.getLength());
-        Vector2D vector2D1 = new Vector2D(1, 5, 4, 9);
         System.out.println(vector2D1.getVectorCords());
         System.out.println(vector2D1.getLength());
         System.out.println(vector2D.getAngle(vector2D1));
         System.out.println(vector2D.getScalarProduct(vector2D1));
+        System.out.println();
+        Vector3D vector3D = new Vector3D(0,0,0,1,1,1);
+        Vector3D vector3D1 = new Vector3D(0,0,0,3,2,0);
+        System.out.println(vector3D.getVectorCords());
+        System.out.println(vector3D.getLength());
+        System.out.println(vector3D1.getVectorCords());
+        System.out.println(vector3D1.getLength());
+        System.out.println(vector3D.getScalarProduct(vector3D1));
     }
 }
--- Module 08 Practic_Week_2/Exercise 6 Vector/src/Vector2D.java
+++ Module 08 Practic_Week_2/Exercise 6 Vector/src/Vector2D.java
@@ -1,28 +1,30 @@
-public class Vector2D extends Vector{
+public class Vector2D extends Vector {
     public Vector2D(int x1, int y1, int x2, int y2) {
         super(x1, y1, 0, x2, y2, 0);
     }
+
     int vectorXCords = getX2() - getX1();
     int vectorYCords = getY2() - getY1();
 
     @Override
-    double getLength() {
+    public double getLength() {
         return Math.abs(Math.sqrt(vectorXCords * vectorXCords + vectorYCords * vectorYCords));
     }
 
     @Override
-    String getVectorCords() {
+    public String getVectorCords() {
         // AB = {Bx - Ax; By - Ay} = {4 - 2; 3 - 3} = {2; 0}
-        return vectorXCords + ", " + vectorYCords;
+        return vectorXCords + " " + vectorYCords;
     }
 
-    double getScalarProduct(Vector2D vector) {
-        return getLength() * vector.getLength() * getAngle(vector);
+    double getScalarProduct(Vector2D vector2D) {
+        return vectorXCords * vector2D.vectorXCords + vectorYCords * vector2D.vectorYCords;
     }
 
-    double getAngle(Vector2D vector) {
-        double abLength = vectorXCords * vector.vectorXCords + vectorYCords * vector.vectorYCords;
-        double abModule = getLength() * vector.getLength();
-        return abLength / abModule;
+    double getAngle(Vector2D vector2D) {
+        if (getLength() != 0 && vector2D.getLength() != 0) {
+            return getScalarProduct(vector2D) / (getLength() * vector2D.getLength());
+        }
+        return -2.0;
     }
 }
