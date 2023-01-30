849aa8041df8c67795f368b3dc5aa2613f33265d
Sergey Tishchenko <seti.volch@gmail.com>
2023-01-30 21:08:20 +0700

    add and complete exercise 7

---
+++ Module 08 Practic_Week_2/Exercise 07 Transport/src/Car.java
@@ -0,0 +1,24 @@
+public class Car extends Transport{
+    int maxPassengers;
+    public Car(int fuel, int speed, int maxPassengers) {
+        super(fuel, speed);
+        this.maxPassengers = maxPassengers;
+    }
+
+    @Override
+    public boolean canMove(int n) {
+        if (fuel >= n) {
+            fuel -= n;
+            return true;
+        } else return false;
+    }
+
+    @Override
+    public int getFuelLevel() {
+        return this.fuel;
+    }
+
+    public int getMaxPassengers() {
+        return maxPassengers;
+    }
+}
