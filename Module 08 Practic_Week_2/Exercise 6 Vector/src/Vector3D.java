36d78fd8840b5accf33241f289ff0f21b9b2c19f
Sergey Tishchenko <seti.volch@gmail.com>
2023-01-30 19:09:44 +0700

    First commit

---
+++ Module 08 Practic_Week_2/Exercise 6 Vector/src/Vector3D.java
@@ -0,0 +1,19 @@
+public class Vector3D extends Vector{
+    public Vector3D(int x1, int y1, int z1, int x2, int y2, int z2) {
+        super(x1, y1, z1, x2, y2, z2);
+    }
+    int vectorXCords = getX2() - getX1();
+    int vectorYCords = getY2() - getY1();
+    int vectorZCords = getZ2() - getZ1();
+
+    @Override
+    double getLength() {
+        return Math.sqrt(vectorXCords * vectorXCords + vectorYCords * vectorYCords + vectorZCords * vectorZCords);
+    }
+
+    @Override
+    String getVectorCords() {
+        // AB = {Bx - Ax; By - Ay; Bz - Az} = {4 - 2; 3 - 3; 2 - 4} = {2; 0; -2}
+        return vectorXCords + ", " + vectorYCords + ", " + vectorZCords;
+    }
+}
