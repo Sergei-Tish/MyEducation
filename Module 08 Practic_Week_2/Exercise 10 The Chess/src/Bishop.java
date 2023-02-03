5e9d4983ec9c786c76b15b762e69ade48a9c67e4
Sergey Tishchenko <seti.volch@gmail.com>
2023-02-03 23:19:57 +0700

    Chess - add for Bishop - so that the figure cannot pass through another

--- Module 08 Practic_Week_2/Exercise 10 The Chess/src/Bishop.java
+++ Module 08 Practic_Week_2/Exercise 10 The Chess/src/Bishop.java
@@ -10,16 +10,55 @@ public class Bishop extends ChessPiece {
 
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        if (checkBorder(toLine, toColumn) &&
-                chessBoard.board[toLine][toColumn] != null &&
+        if (!checkBorder(toLine, toColumn)) return false;
+        if (chessBoard.board[toLine][toColumn] != null &&
                 chessBoard.board[toLine][toColumn].getColor().equals(this.getColor())) {
             return false;
         }
         boolean myPath = false;
+
+        if (toLine > line && toColumn > column) {
+            for (int i = 0; i < 8; i++) {
+                if (toLine == (line + i) && toColumn == (column + i)) {
+                    return true;
+                } else if (chessBoard.board[line + i][column + i] != null) {
+                    return false;
+                }
+            }
+        }
+        if (toLine > line && toColumn < column) {
+            for (int i = 0; i < 8; i++) {
+                if (toLine == (line + i) && toColumn == (column - i)) {
+                    return true;
+                } else if (chessBoard.board[line + i][column - i] != null) {
+                    return false;
+                }
+            }
+        }
+        if (toLine < line && toColumn > column) {
+            for (int i = 0; i < 8; i++) {
+                if (toLine == (line - i) && toColumn == (column + i)) {
+                    return true;
+                } else if (chessBoard.board[line - i][column + i] != null) {
+                    return false;
+                }
+            }
+        }
+        if (toLine < line && toColumn < column) {
+            for (int i = 0; i < 8; i++) {
+                if (toLine == (line - i) && toColumn == (column - i)) {
+                    return true;
+                } else if (chessBoard.board[line - i][column - i] != null) {
+                    return false;
+                }
+            }
+        }
+        /*      -= MY FIRST TRY =-
+        boolean myPath = false;
         for (int i = 1; i < 8; i++) {
             if (checkBorder(toLine, toColumn) &&
                     (
-                            (toLine == (line + i) && toColumn == (column + i)) ||
+                                    (toLine == (line + i) && toColumn == (column + i)) ||
                                     (toLine == (line + i) && toColumn == (column - i)) ||
                                     (toLine == (line - i) && toColumn == (column + i)) ||
                                     (toLine == (line - i) && toColumn == (column - i))
@@ -29,6 +68,9 @@ public class Bishop extends ChessPiece {
             }
         }   // put' ne proveryaet na prepyatstviya
         return myPath;
+
+         */
+        return false;
     }
 
     @Override
