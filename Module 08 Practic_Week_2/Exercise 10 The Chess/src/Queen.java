687d6d4d7d3f3bd09f5502beb4b038ea171b6d88
Sergey Tishchenko <seti.volch@gmail.com>
2023-02-14 23:19:43 +0700

    Chess - figure is not invisible 1

--- Module 08 Practic_Week_2/Exercise 10 The Chess/src/Queen.java
+++ Module 08 Practic_Week_2/Exercise 10 The Chess/src/Queen.java
@@ -11,6 +11,8 @@ public class Queen extends ChessPiece {
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
         if (!checkBorder(toLine, toColumn)) return false;
+        chessBoard.printBoard();
+        System.out.println("int line " + line + ", int column " +column+ ", int toLine "+toLine+ ", int toColumn " + toColumn);
         boolean myPath = false;
         if (chessBoard.board[toLine][toColumn] != null &&
                 chessBoard.board[toLine][toColumn].getColor().equals(this.getColor())) {
@@ -35,38 +37,37 @@ public class Queen extends ChessPiece {
                 }
             }
         }
-        if (toColumn == column && toLine > line) {
+        if (toLine > line && toColumn == column) {
             for (int i = 0; i < 8; i++) {
                 if (toLine == (line + i)) {
                     return true;
-                } else if (chessBoard.board[toLine + i][toColumn] != null) {
+                } else if (chessBoard.board[line + i][toColumn] != null) {
                     return false;
                 }
             }
         }
-        if (toColumn == column && toLine > line) {
+        if (toLine < line && toColumn == column) {
             for (int i = 0; i < 8; i++) {
                 if (toLine == (line - i)) {
                     return true;
-                } else if (chessBoard.board[toLine - i][toColumn] != null) {
+                } else if (chessBoard.board[line - i][toColumn] != null) {
                     return false;
                 }
             }
         }
 
         //for diagonal move
-        if (toLine > line && toColumn > column) {
-            //checkPath(ChessBoard chessBoard, int line, int column, int toLine, int toColumn, char indexLine, char indexColumn)
-            return checkPath(chessBoard, line, column, toLine, toColumn, '>', '>');
+        if (toLine > line && toColumn > column && Math.abs(toLine - line) == Math.abs(toColumn - column)) {
+            //return checkPath(chessBoard, line, column, toLine, toColumn, '>', '>');
             for (int i = 0; i < 8; i++) {
-                if (toLine == (line + (i * 1)) && toColumn == (column + (i * 1))) {
+                if (toLine == (line + (i * 1)) && toColumn == (column + (i * (1)))) {
                     return true;
-                } else if (chessBoard.board[line + (i * 1)][column + (i * 1)] != null) {
+                } else if (chessBoard.board[line + (i * 1)][column + (i * (1))] != null) {
                     return false;
                 }
             }
         }
-        if (toLine > line && toColumn < column) {
+        if (toLine > line && toColumn < column && Math.abs(toLine - line) == Math.abs(column - toColumn)) {
             for (int i = 0; i < 8; i++) {
                 if (toLine == (line + (i * 1)) && toColumn == (column + (i * (-1)))) {
                     return true;
@@ -75,8 +76,8 @@ public class Queen extends ChessPiece {
                 }
             }
         }
-        if (toLine < line && toColumn > column) {
-            for (int i = 0; i < 8; i++) {
+        if (toLine < line && toColumn > column && Math.abs(line - toLine) == Math.abs(toColumn - column)) {
+            for (int i = 1; i < 8; i++) {
                 if (toLine == (line + (i * (-1))) && toColumn == (column + (i * 1))) {
                     return true;
                 } else if (chessBoard.board[line + (i * (-1))][column + (i * 1)] != null) {
@@ -84,32 +85,32 @@ public class Queen extends ChessPiece {
                 }
             }
         }
-        if (toLine < line && toColumn < column) {
-            for (int i = 0; i < 8; i++) {
-                if (toLine == (line + (i * (-1))) && toColumn == (column + (i * (-1)))) {
-                    return true;
-                } else if (chessBoard.board[line + (i * (-1))][column + (i * (-1))] != null) {
-                    return false;
-                }
-            }
+        if (toLine < line && toColumn < column && Math.abs(line - toLine) == Math.abs(column - toColumn)) {
+            return checkPath(chessBoard, line, column, toLine, toColumn, '<' , '<');
         }
 
 
         return myPath;
     }
 
-    private boolean checkPath(ChessBoard chessBoard, int line, int column, int toLine, int toColumn, char indexLine, char indexColumn) {
-        int intIndexLine;
-        switch (indexLine) {
-            case '>' -> intIndexLine = 1;
-            case '<' -> intIndexLine = -1;
-            default -> intIndexLine = 0;
+    private boolean checkPath(ChessBoard chessBoard, int line, int column, int toLine, int toColumn, char ratioLines, char ratioColumns) {
+        int intIndexLine = 0;
+        switch (ratioLines) {
+            case '>':
+                intIndexLine = 1;
+                break;
+            case '<':
+                intIndexLine = -1;
+                break;
         }
-        int intIndexColumn;
-        switch (indexColumn) {
-            case '>' -> intIndexColumn = 1;
-            case '<' -> intIndexColumn = -1;
-            default -> intIndexColumn = 0;
+        int intIndexColumn = 0;
+        switch (ratioColumns) {
+            case '>':
+                intIndexColumn = 1;
+                break;
+            case '<':
+                intIndexColumn = -1;
+                break;
         }
         for (int i = 0; i < 8; i++) {
             if (toLine == (line + (i * intIndexLine)) && toColumn == (column + (i * intIndexColumn))) {
@@ -120,6 +121,7 @@ public class Queen extends ChessPiece {
         }
         return false;
     }
+
     @Override
     public String getSymbol() {
         return "Q";
