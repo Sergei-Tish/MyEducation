8b3b653dc5296f3c09739d4dbfc9108721a88286
Sergey Tishchenko <seti.volch@gmail.com>
2023-02-19 22:26:35 +0700

    add Exercise 9.4.1 JAR.iml and Exercise 9.5.1 Math add 2 method.iml

---
+++ Module 09 Standard libraries/Exercise 9.4.1 JAR/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Соберите свой JAR-файл и запустите его.");
+        System.out.println("Я выполнил это с ForChess2");
+    }
+}
