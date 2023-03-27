4510aded42d0ae61ec604b3f81d31caedf8d8f5d
Sergey Tishchenko <seti.volch@gmail.com>
2023-03-27 23:48:11 +0700

    in 13.3.3 add -name for change Name and -list to show the list online

--- Module 13 Multithreading/Exercise 13.3.3 simple_Chat/src/ChatServer.java
+++ Module 13 Multithreading/Exercise 13.3.3 simple_Chat/src/ChatServer.java
@@ -2,34 +2,48 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ChatServer {
     ArrayList<Client> clients = new ArrayList<>();
     private ServerSocket serverSocket;
 
-    ChatServer () throws IOException { // создаем серверный сокет на порту 1234
+    ChatServer() throws IOException { // создаем серверный сокет на порту 1234
         serverSocket = new ServerSocket(1234);
     }
 
-    void sendAll (String message) {
+    void sendAll(String message) {
         for (Client client : clients) {
             client.send(message);
         }
     }
 
-    public void run () {
+    public void run() {
         System.out.println("Waiting for a client...");
         while (true) {
             try {
                 Socket socket = serverSocket.accept(); // ждем клиента из сети
-                System.out.println("Client connected!");
-                clients.add(new Client(socket, this)); // создаем клиента на своей стороне
+                Client newClient = new Client(socket, this); // создаем клиента на своей стороне
+                clients.add(newClient); // Добавляем его в лист клиентов
+                System.out.println("New client " + newClient.getName() + " connected!");
+                //sendAll("New client " + newClient.getName() + " connected!");
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
+
     public static void main(String[] args) throws IOException {
         new ChatServer().run();
+    }
+
+    void remove(Client clientArg) {
+
+        for (Iterator<Client> iterator = clients.iterator(); iterator.hasNext(); ) {
+            Client client = iterator.next();
+            if (client.getIdClient() == clientArg.getIdClient()) {
+                iterator.remove();
+            }
+        }
     }
 }
--- Module 13 Multithreading/Exercise 13.3.3 simple_Chat/src/Client.java
+++ Module 13 Multithreading/Exercise 13.3.3 simple_Chat/src/Client.java
@@ -3,28 +3,45 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 class Client implements Runnable {
-    Socket socket;
-    ChatServer server;
-    Scanner in;
-    PrintStream out;
+    private Socket socket;
+    private ChatServer server;
+    private Scanner in;
+    private PrintStream out;
+
+    public String getName() {
+        return name;
+    }
+
     private String name;
-    int countOfClient = 0;
+    private static int countOfAnonymous = 0;
+
+    public int getIdClient() {
+        return idClient;
+    }
+
+    private final int idClient;
 
     public Client(Socket socket, ChatServer server){
         this.socket = socket;
         this.server = server;
-        name = "Anonymous_#" + ++countOfClient;
+        name = "Anonymous_" + ++countOfAnonymous;
+        idClient = countOfAnonymous;
         // запускаем поток
         Thread thread = new Thread(this);
         thread.start();
     }
 
     void send (String massage) {
-        out.println(name + ": " + massage);
+        try {
+            out.println(massage);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void run() {
@@ -38,24 +55,55 @@ class Client implements Runnable {
             out = new PrintStream(os);
 
             // читаем из сети и пишем в сеть
-            out.println("Welcome to Our little chat! \n Your name in the chat is \"" + name + "\". \nIf you want change your name, write command -name YOURNAME");
+            out.println("Welcome to Our little chat! Your name in the chat is \"" + name + "\".");
+            out.println("If you want change your name, write command -name YOURNEWNAME");
+            server.sendAll("New client " + name + " connected!");
             String input = in.nextLine();
 
             while (!input.equals("bye")) {
                 if (input.startsWith("-name ")) {
-
-                }
-                server.sendAll(input);
-                try {
-                    input = in.nextLine();
-                } catch (NoSuchElementException e) {
-                    System.out.println("Client " + name + " has disconnected.");
-                    countOfClient--;
+                    changeClientName(input);
+                    try {
+                        input = in.nextLine();
+                    } catch (RuntimeException e) {
+                        break;
+                    }
+                } else if (input.startsWith("-list")) {
+                    serverListOnline();
+                    try {
+                        input = in.nextLine();
+                    } catch (RuntimeException e) {
+                        break;
+                    }
+                } else {
+                    server.sendAll(name + ": " + input);
+                    try {
+                        input = in.nextLine();
+                    } catch (RuntimeException e) {
+                        break;
+                    }
                 }
             }
+            server.sendAll(name + " has disconnected.");
+            System.out.println(name + " has disconnected.");
+            server.remove(this);
             socket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    void serverListOnline() {
+        out.print("Now online: ");
+        for (Client clientFromList : server.clients) {
+            out.print(clientFromList.name + ", ");
+        }
+        out.println();
+    }
+
+    private void changeClientName(String input) {
+        String newName = input.substring(6);
+        server.sendAll(name + " now have new name - \"" + newName + "\"");
+        name = newName;
     }
 }
