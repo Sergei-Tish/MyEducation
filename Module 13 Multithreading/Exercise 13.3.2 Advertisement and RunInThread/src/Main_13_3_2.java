900354532a0921f78ea762b49635ac47d608e442
Sergey Tishchenko <seti.volch@gmail.com>
2023-03-28 00:21:28 +0700

    in 13.3.3 easy correcting

--- Module 13 Multithreading/Exercise 13.3.2 Advertisement and RunInThread/src/Main_13_3_2.java
+++ Module 13 Multithreading/Exercise 13.3.2 Advertisement and RunInThread/src/Main_13_3_2.java
@@ -6,6 +6,15 @@ public class Main_13_3_2 extends Thread{
         Main_13_3_2 main_13_3_2 = new Main_13_3_2();
         main_13_3_2.start();
 
+        ///////////// Задание 13.3.4 ///////
+            // Lunch thread on class Advertisement,
+            // which implements Runnable
+            // (it's hidden)
+
+        new Thread(new Advertisement()).start();
+        ////////////////////////////////////
+
+
 
     }
     public void run() {
