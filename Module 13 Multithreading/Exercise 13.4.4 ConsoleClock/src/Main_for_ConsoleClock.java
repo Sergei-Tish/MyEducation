7a404b1528b7b256bea72e4e31c6ae7bd76b1f1c
Sergey Tishchenko <seti.volch@gmail.com>
2023-03-31 21:20:33 +0700

    in 13.4.4 Thread getId() and threadId() is worked. in class Thread — "public long getId() { return threadId(); }"

--- Module 13 Multithreading/Exercise 13.4.4 ConsoleClock/src/Main_for_ConsoleClock.java
+++ Module 13 Multithreading/Exercise 13.4.4 ConsoleClock/src/Main_for_ConsoleClock.java
@@ -2,9 +2,12 @@ public class Main_for_ConsoleClock {
 
 
     public static void main(String[] args) throws InterruptedException{
-
+        System.out.print(Thread.currentThread().getId() + " ");
+        System.out.println(Thread.currentThread().getName());
         ConsoleClock cc = new ConsoleClock();
         cc.start();
+        System.out.print(cc.threadId() + " ");
+        System.out.println(cc.getName());
         Thread.sleep(3000);
         cc.interrupt();
     }
