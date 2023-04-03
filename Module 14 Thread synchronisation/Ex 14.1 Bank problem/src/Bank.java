ec526c412f8bc22063691b2db7c4040d8fcfa2dc
Sergey Tishchenko <seti.volch@gmail.com>
2023-04-03 20:20:37 +0700

    in 14.1  edit

--- Module 14 Thread synchronisation/Ex 14.1 Bank problem/src/Bank.java
+++ Module 14 Thread synchronisation/Ex 14.1 Bank problem/src/Bank.java
@@ -1,56 +1,36 @@
-public class Bank {
-    private int money = -1000;
+public class Bank extends Thread {
+    private int money = 10000;
 
-    public synchronized void takeCredit(String nameClient) {
-        if (money < 1000) {
-            System.out.println(nameClient + " Not enough money in the bank.");
-        } else {
-            money -= 1000;
-            System.out.println(nameClient + " Credit taken. Bank money: " + money);
-        }
+    public Bank() {
+        new Client().start();
+        new Client().start();
+        new Client().start();
     }
 
-    public synchronized void returnCredit(String nameClient) {
-        money += 1000;
-        System.out.println(nameClient + " Credit returned. Bank money: " + money);
-    }
-
-    public static void main(String[] args) {
-        Bank bank = new Bank();
-
-        for (int i = 0; i < 3; i++) {
-            Client client = new Client(bank);
-            client.start();
-        }
-    }
-}
-
-class Client extends Thread {
-    private Bank bank;
-    String name;
-    static int clientCount = 0;
-    int myCount;
-
-    public Client(Bank bank) {
-        this.bank = bank;
-        name = "Cl.#"+ ++clientCount;
-        myCount = 100 * clientCount;
-        try {
-            sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void run() {
-        while (true) {
-            bank.takeCredit(name);
-            bank.returnCredit(name);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                System.out.println("Thread interrupted");
+    class Client extends Thread{
+        @Override
+        public void run() {
+            while(true) {
+                take(1000);
+                repay(1000);
             }
+        }
+    }
+
+    public synchronized void take(int moneyOperation) {
+            money -= moneyOperation;
+    }
+
+    public synchronized void repay(int moneyOperation) {
+        money += moneyOperation;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Bank bank = new Bank();
+        while (true) {
+
+            sleep(1000);
+            System.out.println(Thread.currentThread().getName() + " Bank money: " + bank.money);
         }
     }
 }
