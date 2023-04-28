6b154f283789aa0670b923622d832107133d688a
Sergey Tishchenko <seti.volch@gmail.com>
2023-04-28 18:40:41 +0700

    add 15.06.00 edit Subpath.java

--- Module 15 lambda functions - Stream API - Date Time API in Java/Ex 15.06.00 printPathInformation/src/Subpath.java
+++ Module 15 lambda functions - Stream API - Date Time API in Java/Ex 15.06.00 printPathInformation/src/Subpath.java
@@ -5,17 +5,19 @@ import java.nio.file.Paths;
 public class Subpath {
     public static void main(String[] args) {
 
-        Path path = Paths.get("/mammal/carnivore/raccoon.image");
-        System.out.println("Path is: " + path);
-
-        System.out.println("Subpath from 0 to 3 is: " + path.subpath(0, 3));
-        System.out.println("Subpath from 1 to 3 is: " + path.subpath(1, 3));
-        System.out.println("Subpath from 1 to 2 is: " + path.subpath(1, 2));
+//        Path path = Paths.get("/mammal/carnivore/raccoon.image");
+//        System.out.println("Path is: " + path);
+//
+//        System.out.println("Subpath from 0 to 3 is: " + path.subpath(0, 3));
+//        System.out.println("Subpath from 1 to 3 is: " + path.subpath(1, 3));
+//        System.out.println("Subpath from 1 to 2 is: " + path.subpath(1, 2));
 
         try {
             System.out.println(Paths.get("K:\\IdeaProjects\\GitTest\\GitTest.java").toRealPath());
             System.out.println(Paths.get("..\\..\\IdeaProjects\\GitTest\\GitTest.java").toRealPath());
+            System.out.println(Paths.get("..\\Hello\\Hello.iml").toRealPath());
             System.out.println(Paths.get("..\\Hel.iml").toRealPath());
+            System.out.println(Paths.get("..\\Hello\\Hello.iml").toRealPath());
             System.out.println(Paths.get("..\\Hello\\Hello.iml").toRealPath());
         } catch (IOException e) {
             // обработка ошибки
