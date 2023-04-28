86e137dbd2ffe0dbee23ce52fa3f5026bc542fe6
Sergey Tishchenko <seti.volch@gmail.com>
2023-04-28 20:01:05 +0700

    add 15.06.00 add Main_Files_copy.java

---
+++ Module 15 lambda functions - Stream API - Date Time API in Java/Ex 15.06.00 printPathInformation/src/Main_Files_copy.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main_Files_copy {
+
+    public static void main(String[] args) {
+
+
+        for (int i = 2; i < 10; i++) {
+            try {
+                Files.copy(Paths.get("D:\\Sergey\\Pictures\\Memasiki\\screen1.jpg"), Paths.get("D:\\Sergey\\Pictures\\Memasiki\\screen" + i + ".jpg"));
+    //            Files.copy(Paths.get("panda/bamboo.txt"), Paths.get("panda-save/bamboo.txt"));
+            } catch (IOException e) {
+                // Handle file I/O exception
+                System.out.println("EXCEPTION: " + e);
+            }
+        }
+
+    }
+
+}
