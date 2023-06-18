447b4a80a07e8b51ef1f5195fddeb6de41f9ede7
Sergey Tishchenko <seti.volch@gmail.com>
2023-06-18 17:52:04 +0700

    продолжаем изучать Базы данных, многие ко многим и т.п.

--- Module 16 Java Memory Model and Garbage Collection/src/RandomTesting.java
+++ Module 16 Java Memory Model and Garbage Collection/src/RandomTesting.java
@@ -3,4 +3,4 @@ public class RandomTesting {
 //aasd
     // практикую MySQL в MySQL 8.0 Command Line Client
     // 11.06.2023. установка и запуск PostgreSQL
-}
+    // 18.06.2023. продолжаем изучать Базы данных, многие ко многим и т.п.
