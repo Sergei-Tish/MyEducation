bbac92f509635c7db8fd070e15a0f0d21c70e17f
Sergey Tishchenko <seti.volch@gmail.com>
2023-06-24 15:35:48 +0700

    Медленное обучение PostgreSQL

--- Module 16 Java Memory Model and Garbage Collection/src/RandomTesting.java
+++ Module 16 Java Memory Model and Garbage Collection/src/RandomTesting.java
@@ -1,6 +1,8 @@
 public class RandomTesting {
 
 //aasd
+    // Модуль 17 MySQL и базы данных
     // практикую MySQL в MySQL 8.0 Command Line Client
     // 11.06.2023. установка и запуск PostgreSQL
     // 18.06.2023. продолжаем изучать Базы данных, многие ко многим и т.п.
+    // 24.06.2023. Медленное обучение PostgreSQL
