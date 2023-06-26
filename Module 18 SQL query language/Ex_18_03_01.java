952f5c00a1c46acb6695057c1c556c693f520905
Sergey Tishchenko <seti.volch@gmail.com>
2023-06-26 19:47:43 +0700

    Медленное обучение PostgreSQL

---
+++ Module 18 SQL query language/Ex_18_03_01.java
@@ -0,0 +1,9 @@
+public class Ex_18_03_01 {
+    SELECT
+    name AS "Название товара",
+    price AS "Цена",
+    category AS "Категория",
+    in_stock AS "В наличии"
+    FROM
+            products;
+}
