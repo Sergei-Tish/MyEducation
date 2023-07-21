b225675e402d54ef6c0839f5b9f5a3084a1ddd81
Sergey Tishchenko <seti.volch@gmail.com>
2023-07-21 23:49:36 +0700

    обучение PostgreSQL Module 20 Advanced use of relational database 21/07/23

--- Module 20 Advanced use of relational database/new_SQL_queries.java
+++ Module 20 Advanced use of relational database/new_SQL_queries.java
@@ -1,10 +1,24 @@
 
-                    /**         20.1. Индексы       */
+                    /**         20.1. Индексы           */
 
-Задание 20.1.2{
+Задание 20.1.2 {
         /** Создайте составной индекс (категория и цена) для таблицы с продуктами.                                      */
         CREATE INDEX indx_categ_prc ON products(category,price);                                                        }
 
 Задание 20.1.3 {
         /** Создайте упорядоченный по убыванию индекс(по дате)для заказов.Значения NULL должны быть на первом месте.    */
         create index indx_orders_date on orders(date desc nulls first);                                                 }
+                    /**         20.2. Триггеры          */
+Задание 20.2.1 {
+        /** Напишите код, который создаёт построчный BEFORE триггер для описанной выше функции.                         */
+        CREATE TRIGGER emp_stamp BEFORE INSERT OR UPDATE
+            ON emp FOR EACH ROW EXECUTE PROCEDURE emp_stamp();                                                          }
+                    /**         20.3. Представления      */
+
+
+
+
+
+
+
+
