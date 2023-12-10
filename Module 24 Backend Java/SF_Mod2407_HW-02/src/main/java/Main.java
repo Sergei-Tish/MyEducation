6b4e5d4219f101febe2844d476223b1eff4a02da
Sergey Tishchenko <seti.volch@gmail.com>
2023-12-10 21:14:56 +0700

    обучение Maven - 24.7  10/12/23

---
+++ Module 24 Backend Java/SF_Mod2407_HW-02/src/main/java/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        University university = new University("0", "АлтГосУнивер", "АГУ", 1901, StudyProfile.PEDAGOGY);
+
+        System.out.println(university);
+    }
+}
