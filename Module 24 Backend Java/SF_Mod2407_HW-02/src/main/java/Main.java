506a9cad52c32aa233fae6d6e732ecdd4b76deb8
Sergey Tishchenko <seti.volch@gmail.com>
2023-12-13 20:31:01 +0700

    обучение Maven - 24.7  13/12/23

--- Module 24 Backend Java/SF_Mod2407_HW-02/src/main/java/Main.java
+++ Module 24 Backend Java/SF_Mod2407_HW-02/src/main/java/Main.java
@@ -2,8 +2,16 @@
 
 public class Main {
     public static void main(String[] args) {
-        University university = new University("0", "АлтГосУнивер", "АГУ", 1901, StudyProfile.PEDAGOGY);
+        University university = new University("0", "АлтайГосУнивер", "АГУ");
+        University university1 = new University("1", "АлтайГосТехнУнивер", "АГТУ", 1702);
+        University university2 = new University("2", "НовосГосТехнУнивер", "НГТУ", 1803, StudyProfile.COMPUTER_SCIENCE);
+        University university3 = new University("3", "МосковГосМедУнивер", "МГМУ", 1904, StudyProfile.MEDICINE);
 
         System.out.println(university);
+        System.out.println(university1);
+        System.out.println(university2);
+        System.out.println(university3);
+
+
     }
 }
