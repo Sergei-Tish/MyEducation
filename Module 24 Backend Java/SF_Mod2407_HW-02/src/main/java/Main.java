5f87e0078ce60b9df9e8f3af10e5576e7e1494e8
Sergey Tishchenko <seti.volch@gmail.com>
2023-12-17 23:13:07 +0700

    обучение Maven - 24.7  17/12/23

--- Module 24 Backend Java/SF_Mod2407_HW-02/src/main/java/Main.java
+++ Module 24 Backend Java/SF_Mod2407_HW-02/src/main/java/Main.java
@@ -2,16 +2,25 @@
 
 public class Main {
     public static void main(String[] args) {
-        University university = new University("0", "АлтайГосУнивер", "АГУ");
+        University university0  = new University("0", "АлтайГосУнивер", "АГУ");
         University university1 = new University("1", "АлтайГосТехнУнивер", "АГТУ", 1702);
         University university2 = new University("2", "НовосГосТехнУнивер", "НГТУ", 1803, StudyProfile.COMPUTER_SCIENCE);
         University university3 = new University("3", "МосковГосМедУнивер", "МГМУ", 1904, StudyProfile.MEDICINE);
 
-        System.out.println(university);
+        System.out.println(university0);
         System.out.println(university1);
         System.out.println(university2);
         System.out.println(university3);
 
+        Student student  = new Student("Tishchenko Sergey Ivanovich");
+        Student student1 = new Student("Tishchenko Valeria Olegovna", "0");
+        Student student2 = new Student("YAzvenko Lera doch'Olega", "3", 3);
+        Student student3 = new Student("Elderrus Drocheslav synSergeya", "2", 2, 3.3f);
+
+        System.out.println(student);
+        System.out.println(student1);
+        System.out.println(student2);
+        System.out.println(student3);
 
     }
 }
--- Module 24 Backend Java/SF_Mod2407_HW-02/src/main/java/Student.java
+++ Module 24 Backend Java/SF_Mod2407_HW-02/src/main/java/Student.java
@@ -1,8 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Student {
     private String fullName;
     private String universityId;
     private int currentCourseNumber;
     private float avgExamScore;
+
+    private static Map<String, String> universityData = new HashMap<>();
+    public static Map<String, String> getUniversityData(String universityId) {
+        return universityData;
+    }
+    public static void setUniversityData(Map<String, String> universityData) {
+        Student.universityData = Student.getUniversityData() + universityData;
+    }
 
     public Student(String fullName) {
         this.fullName = fullName;
@@ -16,6 +27,10 @@ public class Student {
     public Student(String fullName, String universityId, int currentCourseNumber) {
         this(fullName, universityId);
         this.currentCourseNumber = currentCourseNumber;
+    }
+    public Student(String fullName, String universityId, int currentCourseNumber, float avgExamScore) {
+        this(fullName, universityId, currentCourseNumber);
+        this.avgExamScore = avgExamScore;
     }
 
     public String getFullName() {
@@ -33,6 +48,8 @@ public class Student {
     public float getAvgExamScore() {
         return avgExamScore;
     }
+
+
 
     public Student setFullName(String fullName) {
         this.fullName = fullName;
@@ -54,14 +71,36 @@ public class Student {
         return this;
     }
 
+
+
     @Override
+//    public String toString() {
+//        String courseInfo = (this.getCurrentCourseNumber() == 0) ? "" : " студент " + this.getCurrentCourseNumber() + " курса";
+//        String universityInfo = (this.getUniversityId() == null) ? "" : " " + this.getUniversityId() + " университета";
+//        String avgScoreInfo = (this.getAvgExamScore() == 0.0) ? "" : " Средний бал " + this.getAvgExamScore();
+//        return this.getFullName() + courseInfo + universityInfo + "." + avgScoreInfo;
+//    }
+
     public String toString() {
-        return this.getFullName()
-                + " "
-                + " cтудент " + this.getCurrentCourseNumber()
-                + " курса " + this.getUniversityId()
-                + "университета."
-                + " Средний бал " + this.getAvgExamScore()
-                ;
+        String courseInfo = (this.getCurrentCourseNumber() == 0) ? "" : " студент " + this.getCurrentCourseNumber() + " курса";
+        String universityInfo = (this.getUniversityId() == null) ? "" : " " + universityShortNameById(this.getUniversityId()) + " университета";
+        String avgScoreInfo = (this.getAvgExamScore() == 0.0) ? "" : " Средний бал " + this.getAvgExamScore();
+        return this.getFullName() + courseInfo + universityInfo + "." + avgScoreInfo;
     }
+
+    private String universityShortNameById(String universityId) {
+        return
+        // Ваша логика получения shortName университета по его id
+    }
+
+
+//    public String toString() {
+//        return this.getFullName()
+//                + " "
+//                + " cтудент " + this.getCurrentCourseNumber()
+//                + " курса " + this.getUniversityId()
+//                + "университета."
+//                + " Средний бал " + this.getAvgExamScore()
+//                ;
+//    }
 }
