2ca0cb0a11fdf75b246610c4bbe0924ab843071f
Sergei Tishchenko <seti.volch@gmail.com>
2024-02-27 18:42:03 +0700

    обучение - after Module 26 - Practice Week 3 - поработал с Google Gson 27/02/24

--- Module 26 Application_design_SOLID_and_beyond/SF_Mod260901_Project/src/main/java/Boot.java
+++ Module 26 Application_design_SOLID_and_beyond/SF_Mod260901_Project/src/main/java/Boot.java
@@ -4,6 +4,7 @@ import io.XlsReader;
 import model.Student;
 import model.University;
 import util.ComparatorUtil;
+import util.JsonUtil;
 
 import java.io.IOException;
 import java.util.Comparator;
@@ -17,7 +18,7 @@ public class Boot {
         List<model.University> universities = XlsReader.readXlsUniversities(filePath);
         List<model.Student> students = XlsReader.readXlsStudents(filePath);
 
-        /*вывод использованием цикла и до сортировки.*/
+        /* вывод использованием цикла и до сортировки. */
         for (University u :
                 universities) {
             System.out.println(u);
@@ -27,28 +28,43 @@ public class Boot {
             System.out.println(s);
         }
 
-        /*Вывести университет опредедленного студента.*/
-/*
-        Student studentForPrintUniversity = students.get(3);
-        System.out.println("\n" + "Вывести университет студента " + studentForPrintUniversity.getFullName());
-        for (University u :
-                universities) {
-            if (u.getId().equals(studentForPrintUniversity.getUniversityId())) {
-                System.out.println(u);
-            }
-        }
-*/
-        /*создание нужных компараторов для сортировки*/
-        Comparator<University> universityComparator = ComparatorUtil.getComparator(UniversityComparatorType.YEAR);
-        Comparator<Student> studentComparator = ComparatorUtil.getComparator(StudentComparatorType.AVG_EXAM_SCORE);
+//        /* Вывести университет опредедленного студента. */
+//
+//        Student studentForPrintUniversity = students.get(3);
+//        System.out.println("\n" + "Вывести университет студента " + studentForPrintUniversity.getFullName());
+//        for (University u :
+//                universities) {
+//            if (u.getId().equals(studentForPrintUniversity.getUniversityId())) {
+//                System.out.println(u);
+//            }
+//        }
 
-        System.out.println();
-        /*вывод использованием Stream API и после сортировки.*/
-        universities.stream().
-                sorted(universityComparator).
-                forEach(System.out::println);
-        students.stream().
-                sorted(studentComparator).
-                forEach(System.out::println);
+//        /* создание нужных компараторов для сортировки */ //
+//        Comparator<University> universityComparator = ComparatorUtil.getComparator(UniversityComparatorType.YEAR);
+//        Comparator<Student> studentComparator = ComparatorUtil.getComparator(StudentComparatorType.AVG_EXAM_SCORE);
+//
+//        System.out.println();
+//        /* вывод использованием Stream API и после сортировки. */
+//        universities.stream().
+//                sorted(universityComparator).
+//                forEach(System.out::println);
+//        students.stream().
+//                sorted(studentComparator).
+//                forEach(System.out::println);
+
+        /* Практическая неделя 3 */
+        System.out.println("\nПрактическая неделя 3 - работа с JSON:");
+        University universityForJson = universities.get(2);
+        String jsonUniversity = JsonUtil.universityToJson(universityForJson);
+        System.out.println("Инфа об универе " + universityForJson.getShortName() + " " + jsonUniversity);
+        Student studentForJson = students.get(3);
+        String jsonStudent = JsonUtil.studentToJson(studentForJson);
+        System.out.println("Инфа о студенте " + studentForJson.getFullName() + " " + jsonStudent);
+
+        String jsonUniversityList = JsonUtil.universityListToJson(universities);
+        System.out.println("Инфа об универах " + jsonUniversityList);
+        String jsonStudentList = JsonUtil.studentListToJson(students);
+        System.out.println("Инфа о студентах " + jsonStudentList);
+
     }
 }
---
+++ Module 26 Application_design_SOLID_and_beyond/SF_Mod260901_Project/src/main/java/util/JsonUtil.java
@@ -0,0 +1,67 @@
+package util;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import model.Student;
+import model.University;
+
+import java.util.List;
+
+public class JsonUtil {
+    private JsonUtil() {
+    }
+
+    /**
+     * В классе JsonUtil реализовать 4 метода сериализации в JSON и 4 метода десериализации из JSON в объекты:
+     * сериализация/десериализация отдельных объектов студентов и университетов,
+     * сериализация/десериализация коллекций этих объектов.
+     * Сериализация должна выполняться в форматированном виде (PrettyPrinting).
+     */
+
+    private static Gson getGsonFromGsonBuilderPrettyPrinting() {
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+        return gson;
+    }
+
+    public static String universityToJson(University university) {
+        Gson gson = getGsonFromGsonBuilderPrettyPrinting();
+        return gson.toJson(university);
+    }
+
+    public static String studentToJson(Student student) {
+        Gson gson = getGsonFromGsonBuilderPrettyPrinting();
+        return gson.toJson(student);
+    }
+
+    public static String universityListToJson(List<model.University> universityList) {
+        Gson gson = getGsonFromGsonBuilderPrettyPrinting();
+        return gson.toJson(universityList);
+    }
+
+    public static String studentListToJson(List<model.Student> studentList) {
+        Gson gson = getGsonFromGsonBuilderPrettyPrinting();
+        return gson.toJson(studentList);
+    }
+
+    public static model.University jsonToUniversity(String json) {
+        return new Gson().fromJson(json, model.University.class);
+    }
+
+    public static model.Student jsonToStudent(String json) {
+        return new Gson().fromJson(json, model.Student.class);
+    }
+
+    public static List<model.University> jsonToUniversityList(String json) {
+        return new Gson().fromJson(json, new TypeToken<List<model.University>>(){}.getType());
+    }
+
+
+    public static List<model.Student> jsonToStudentList(String json) {
+        return new Gson().fromJson(json, new TypeToken<List<model.Student>>(){}.getType());
+    }
+
+
+}
