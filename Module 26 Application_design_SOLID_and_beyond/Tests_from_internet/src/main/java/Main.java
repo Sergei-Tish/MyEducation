989ec1d906ea1363245c9d1bb95de0e99ffd8b03
Sergei Tishchenko <seti.volch@gmail.com>
2024-04-06 21:30:18 +0700

    обучение - Module 27 GoF patterns: Создание и заполнение нового файла Excel с помощью Apache POI — StatisticsUtil 06/04/24

--- Module 26 Application_design_SOLID_and_beyond/Tests_from_internet/src/main/java/Main.java
+++ Module 26 Application_design_SOLID_and_beyond/Tests_from_internet/src/main/java/Main.java
@@ -1,17 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
         test1();
+        test2();
     }
+
+
+    Main(String fullName) {
+        this.fullName = fullName;
+    }
+    String fullName;
+    private String getFullName() {
+        return fullName;
+    }
+
+
     public static void test1() {
         String i = "1";
 
         inc(i);
         System.out.println(i);
     }
-
     private static void inc(String s) {
         s = s + "2";
     }
+
+    public static void test2() {
+        List<Main> universities = new ArrayList<>();
+        Main main = new Main("s");
+        Main main1 = new Main("s1");
+        Main main2 = new Main("s2");
+        Main main3 = new Main("s3");
+        Main main4 = new Main("s4");
+        universities.add(main);
+        universities.add(main1);
+        universities.add(main2);
+        universities.add(main3);
+        universities.add(main4);
+
+        String names = universities.stream()
+                .map(Main::getFullName)
+                .collect(Collectors.joining("; "));
+        System.out.println("-=" + names + "=-");
+    }
+
+
 
 
 }
--- Module 27 Patterns GRASP GoF and etc/SF_Mod270801_Univ_Stud/src/main/java/util/StatisticsUtil.java
+++ Module 27 Patterns GRASP GoF and etc/SF_Mod270801_Univ_Stud/src/main/java/util/StatisticsUtil.java
@@ -7,16 +7,62 @@ import model.University;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StatisticsUtil {
 
     private StatisticsUtil() {
     }
 
-    public static List<Statistics> createStatisticsList (List<Student> Students, List<University> universities) {
+    public static List<Statistics> createStatisticsList(List<Student> students,
+                                                        List<University> universities) {
         List<Statistics> statisticsList = new ArrayList<>();
 
-//        // неверный путь, надо через teamAPI
+        List<StudyProfile> studyProfiles = universities.stream()
+                .map(University::getMainProfile)  // список профилей
+                .distinct() // уникальные профили
+                .collect(Collectors.toList()); // сбор в список
+
+        studyProfiles.forEach(studyProfile -> {
+            Statistics statistics = new Statistics();
+            statisticsList.add(statistics);
+            statistics.setStudyProfile(studyProfile);
+
+            List<String> profileUniversityIds = universities.stream()
+                    .filter(university -> university.getMainProfile().equals(studyProfile))
+                    .map(University::getId)
+                    .collect(Collectors.toList());
+
+            long universitiesCount = profileUniversityIds.stream()
+                    .count();
+            statistics.setNumberOfUniversities((int) universitiesCount);
+
+            String names = universities.stream()
+                    .map(University::getFullName)
+                    .collect(Collectors.joining("; "));
+            statistics.setUniversityNames(names);
+
+            int studentsCount = (int) students.stream()
+                    .filter(student -> student.getUniversityId().equals(
+                            universities.stream().
+                                    filter(university -> university.getMainProfile().equals(studyProfile))
+                    ))
+                    .count();
+            statistics.setNumberOfStudents(studentsCount);
+
+
+        });
+
+
+
+
+        return statisticsList;
+    }
+
+//    public static List<Statistics> createStatisticsList (List<Student> Students, List<University> universities) {
+//        List<Statistics> statisticsList = new ArrayList<>();
+//
+//        // неверный путь, надо через StreamAPI
 //        for (University university: universities) {
 //            StudyProfile studyProfile = university.getMainProfile();
 //            float avgExamScore = 0.0f;
@@ -36,17 +82,44 @@ public class StatisticsUtil {
 //                    .setUniversityNames(universityNames);
 //            statisticsList.add(statistics);
 //        }
-
-        return statisticsList;
-    }
-
-    public static boolean containsStudyProfile(List<Statistics> statisticsList, StudyProfile studyProfileToCheck) {
-        for (Statistics statistics : statisticsList) {
-            if (statistics.getStudyProfile() == studyProfileToCheck) {
-                return true;
-            }
-        }
-        return false;
-    }
+//
+//        return statisticsList;
+//    }
+//
+//    public static boolean containsStudyProfile(List<Statistics> statisticsList, StudyProfile studyProfileToCheck) {
+//        for (Statistics statistics : statisticsList) {
+//            if (statistics.getStudyProfile() == studyProfileToCheck) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
