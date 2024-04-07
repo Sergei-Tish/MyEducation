45bccbee42665d44327746fc89894ea069bfbb27
Sergei Tishchenko <seti.volch@gmail.com>
2024-04-07 21:25:22 +0700

    обучение - Module 27.8 Создание и заполнение нового файла Excel с помощью Apache POI — XlsWriter 07/04/24

--- Module 27 Patterns GRASP GoF and etc/SF_Mod270801_Univ_Stud/src/main/java/io/XlsWriter.java
+++ Module 27 Patterns GRASP GoF and etc/SF_Mod270801_Univ_Stud/src/main/java/io/XlsWriter.java
@@ -1,5 +1,7 @@
 package io;
 
+/** 27.8. Проект */
+
 import model.Statistics;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
@@ -8,13 +10,16 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 public class XlsWriter {
     private XlsWriter() {
     }
 
-    public static void writeXlsStatistics(List<Statistics> statisticsList, String filePath) {
+    public static void writeXlsStatistics(List<Statistics> statisticsList,
+                                          String filePath) throws IOException {
         XSSFWorkbook workbook = new XSSFWorkbook();
         XSSFSheet statisticsSheet = workbook.createSheet("Статистика");
 
@@ -22,25 +27,34 @@ public class XlsWriter {
 
         Font font = workbook.createFont();
         font.setBold(true);
-        font.setFontHeight((short) 13);
         CellStyle cellStyle = workbook.createCellStyle();
         cellStyle.setFont(font);
 
-        Row headerRow = statisticsSheet.createRow(0);
-//        Cell cell = headerRow.createCell(0);  // как указано в решении
-//        cell.setCellValue("Профиль");
-//        cell.setCellStyle(cellStyle);
+        Row headerRow = statisticsSheet.createRow(rowNum++);
         headerRow.createCell(0).setCellValue("Профиль");
         headerRow.createCell(1).setCellValue("Сред.оценка");
         headerRow.createCell(2).setCellValue("Число студентов");
         headerRow.createCell(3).setCellValue("Число унив.");
         headerRow.createCell(4).setCellValue("Названия унив.");
-
-        for (Cell cellInRow : // пробую не как в решении, а перебором
+        for (Cell cellInRow :
                 headerRow) {
             cellInRow.setCellStyle(cellStyle);
         }
 
+        for (Statistics statistics :
+                statisticsList) {
+            Row row = statisticsSheet.createRow(rowNum++);
+            row.createCell(0).setCellValue(statistics.getStudyProfile().getProfileName());
+            row.createCell(1).setCellValue(statistics.getAvgExamScore());
+            row.createCell(2).setCellValue(statistics.getNumberOfStudents());
+            row.createCell(3).setCellValue(statistics.getNumberOfUniversities());
+            row.createCell(4).setCellValue(statistics.getUniversityNames());
+        }
 
+
+
+        try (FileOutputStream outputStream = new FileOutputStream(filePath)) {
+            workbook.write(outputStream);
+        }
     }
 }
