28bbce7046811c4d48b580a78d98234e06c56d98
Sergei Tishchenko <seti.volch@gmail.com>
2024-04-07 21:24:47 +0700

    обучение - Module 27.8 Создание и заполнение нового файла Excel с помощью Apache POI — XlsWriter 07/04/24

--- Module 27 Patterns GRASP GoF and etc/SF_Mod270801_Univ_Stud/src/main/java/Boot.java
+++ Module 27 Patterns GRASP GoF and etc/SF_Mod270801_Univ_Stud/src/main/java/Boot.java
@@ -18,9 +18,10 @@ public class Boot {
 
     public static void main(String[] args) throws IOException {
 
-        String filePath = "K:\\IdeaProjects\\MyEducation\\Module 26 Application_design_SOLID_and_beyond\\SF_Mod260901_Project\\src\\main\\resources\\universityInfo.xlsx";
-        List<University> universities = XlsReader.readXlsUniversities(filePath);
-        List<Student> students = XlsReader.readXlsStudents(filePath);
+        String filePathInput = "K:\\IdeaProjects\\MyEducation\\Module 26 Application_design_SOLID_and_beyond\\SF_Mod260901_Project\\src\\main\\resources\\universityInfo.xlsx";
+        String filePathOutput = "K:\\IdeaProjects\\MyEducation\\Module 26 Application_design_SOLID_and_beyond\\SF_Mod260901_Project\\src\\main\\resources\\universityStatistics.xlsx";
+        List<University> universities = XlsReader.readXlsUniversities(filePathInput);
+        List<Student> students = XlsReader.readXlsStudents(filePathInput);
 
         /* вывод использованием цикла и до сортировки. */
         for (University u :
@@ -32,13 +33,13 @@ public class Boot {
             System.out.println(s);
         }
 
-        /* создание нужных компараторов для сортировки */ //
+        /** создание нужных компараторов для сортировки */ //
         UniversityComparator universityComparator = ComparatorUtil.getComparator(UniversityComparatorType.YEAR);
         StudentComparator studentComparator = ComparatorUtil.getComparator(StudentComparatorType.AVG_EXAM_SCORE);
         universities.sort(universityComparator);
         students.sort(studentComparator);
 
-//        /* Вывести университет опредедленного студента. */
+//        /** Вывести университет опредедленного студента. */
 //
 //        Student studentForPrintUniversity = students.get(3);
 //        System.out.println("\n" + "Вывести университет студента " + studentForPrintUniversity.getFullName());
@@ -51,41 +52,42 @@ public class Boot {
 //        System.out.println();
 
 
-        /* Практическая неделя 3 */
+        /** Практическая неделя 3 */
         System.out.println("\nПрактическая неделя 3 - работа с JSON:");
 //      4. В методе main выполнить сериализацию коллекций 5. и после десериализацию
         String jsonUniversityList = JsonUtil.universityListToJson(universities);    // List<University> в JSON
-        System.out.println("Инфа о ВСЕХ универах в JSON - " + jsonUniversityList);
+//        System.out.println("Инфа о ВСЕХ универах в JSON - " + jsonUniversityList);                // печать JSON универов
         List<University> universitiesFromJson = JsonUtil.jsonToUniversityList(jsonUniversityList);    // JSON обратно в List<University>
 
         String jsonStudentList = JsonUtil.studentListToJson(students);              // List<Student> в JSON
-        System.out.println("Инфа о ВСЕХ студентах в JSON - " + jsonStudentList);
+//        System.out.println("Инфа о ВСЕХ студентах в JSON - " + jsonStudentList);                  // печать JSON студентов
         List<Student> studentsFromJson = JsonUtil.jsonToStudentList(jsonStudentList);                 // JSON обратно в List<Student>
 
-//      6. Сравнить количество элементов в исходной и в десериализованной коллекциях, чтобы убедиться, что десериализация выполняется корректно.
-//        Сравнил полностью - сначала выдало false поэтому переопределил equals() в классах
+        /** 6. Сравнить количество элементов в исходной и в десериализованной коллекциях, чтобы убедиться, что десериализация выполняется корректно.*/
+
+        /**      — Сравнил полностью - сначала выдало false поэтому переопределил equals() в классах */
         System.out.println("Лист универов до JSON и после равны? Ответ: " + universities.equals(universitiesFromJson) + universitiesFromJson.equals(universities) + ".\n" +
-                        "Лист студенттов до JSON и после равны? Ответ: " + students.equals(studentsFromJson) + studentsFromJson.equals(students) + ".\n"
+                "Лист студенттов до JSON и после равны? Ответ: " + students.equals(studentsFromJson) + studentsFromJson.equals(students) + ".\n"
         );
 
-        System.out.println("JSON-строки университетов:");
-        universities.stream()
-                .forEach(university -> {
-                    String jsonUniversity = JsonUtil.universityToJson(university);
-                    System.out.println(jsonUniversity);
-                    University universityFromJson = JsonUtil.jsonToUniversity(jsonUniversity);
-                    System.out.println(universityFromJson);
-                });
-        System.out.println("JSON-строки студентов:");
-        students.stream()
-                .forEach(student -> {
-                    String jsonStudent = JsonUtil.studentToJson(student);
-                    System.out.println(jsonStudent);
-                    Student studentFromJson = JsonUtil.jsonToStudent(jsonStudent);
-                    System.out.println(studentFromJson);
-                });
-
+//        System.out.println("JSON-строки университетов:");
+//        universities.stream()
+//                .forEach(university -> {
+//                    String jsonUniversity = JsonUtil.universityToJson(university);
+//                    System.out.println(jsonUniversity);
+//                    University universityFromJson = JsonUtil.jsonToUniversity(jsonUniversity);
+//                    System.out.println(universityFromJson);
+//                });
+//        System.out.println("JSON-строки студентов:");
+//        students.stream()
+//                .forEach(student -> {
+//                    String jsonStudent = JsonUtil.studentToJson(student);
+//                    System.out.println(jsonStudent);
+//                    Student studentFromJson = JsonUtil.jsonToStudent(jsonStudent);
+//                    System.out.println(studentFromJson);
+//                });
+    /** 27.8. Проект */
         List<Statistics> statisticsList = StatisticsUtil.createStatisticsList(students, universities);
-        XlsWriter.writeXlsStatistics(statisticsList, "K:\\IdeaProjects\\MyEducation\\Module 26 Application_design_SOLID_and_beyond\\SF_Mod260901_Project\\src\\main\\resources\\universityInfo.xlsx");
+        XlsWriter.writeXlsStatistics(statisticsList, filePathOutput);
     }
 }
--- Module 27 Patterns GRASP GoF and etc/SF_Mod270801_Univ_Stud/src/main/java/util/StatisticsUtil.java
+++ Module 27 Patterns GRASP GoF and etc/SF_Mod270801_Univ_Stud/src/main/java/util/StatisticsUtil.java
@@ -1,5 +1,7 @@
 package util;
 
+/** 27.8. Проект */
+
 import enums.StudyProfile;
 import model.Statistics;
 import model.Student;
@@ -32,73 +34,33 @@ public class StatisticsUtil {
                     .filter(university -> university.getMainProfile().equals(studyProfile))
                     .map(University::getId)
                     .collect(Collectors.toList());
-
             long universitiesCount = profileUniversityIds.stream()
                     .count();
             statistics.setNumberOfUniversities((int) universitiesCount);
 
             String names = universities.stream()
+                    .filter(university -> university.getMainProfile().equals(studyProfile))
                     .map(University::getFullName)
                     .collect(Collectors.joining("; "));
             statistics.setUniversityNames(names);
 
-            int studentsCount = (int) students.stream()
-                    .filter(student -> student.getUniversityId().equals(
-                            universities.stream().
-                                    filter(university -> university.getMainProfile().equals(studyProfile))
-                    ))
-                    .count();
+            List<Student> profileStudents = students.stream()
+                    .filter(student -> profileUniversityIds.contains(student.getUniversityId()))
+                    .collect(Collectors.toList());
+            int studentsCount = profileStudents.size();
             statistics.setNumberOfStudents(studentsCount);
 
-
+            float avgExamScore = (float) profileStudents.stream()
+                    .mapToDouble(Student::getAvgExamScore)
+                    .average()
+                    .orElse(0.0);
+            float roundedAvgExamScore = (float) (Math.round(avgExamScore * 100.0) / 100.0);
+            statistics.setAvgExamScore(roundedAvgExamScore);
         });
-
-
-
 
         return statisticsList;
     }
-
-//    public static List<Statistics> createStatisticsList (List<Student> Students, List<University> universities) {
-//        List<Statistics> statisticsList = new ArrayList<>();
-//
-//        // неверный путь, надо через StreamAPI
-//        for (University university: universities) {
-//            StudyProfile studyProfile = university.getMainProfile();
-//            float avgExamScore = 0.0f;
-//            int numberOfStudents = 0;
-//            int numberOfUniversities = 0;
-//            String universityNames = "";
-//
-//            if (containsStudyProfile(statisticsList, studyProfile)) {
-//
-//            }
-//
-//            Statistics statistics = new Statistics()
-//                    .setStudyProfile(studyProfile)
-//                    .setAvgExamScore(avgExamScore)
-//                    .setNumberOfStudents(numberOfStudents)
-//                    .setNumberOfUniversities(numberOfUniversities)
-//                    .setUniversityNames(universityNames);
-//            statisticsList.add(statistics);
-//        }
-//
-//        return statisticsList;
-//    }
-//
-//    public static boolean containsStudyProfile(List<Statistics> statisticsList, StudyProfile studyProfileToCheck) {
-//        for (Statistics statistics : statisticsList) {
-//            if (statistics.getStudyProfile() == studyProfileToCheck) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-
 }
-
-
-
 
 
 
